ef34beb6c4b2140f295d4b4f6362cb587bd9e796
Ryan Sprott <rds2184@tc.columbia.edu>
2019-02-12 11:05:50 -0500

    Extra cells for grid. Pretty colors. Added subunit property to cell.

--- grid_test/cell.js
+++ grid_test/cell.js
@@ -1,54 +1,54 @@
+"use strict";
+
 class Cell {
   constructor(x, y, size, subcells) {
     this.x        = x
     this.y        = y
     this.size     = size
     this.subcells = subcells
+    this.subunit  = size / subcells
   }
 
   show() {
     for (let i = 1; i <= this.subcells; i++) {
       for (let j = 1; j <= this.subcells; j++) {
-        this.subgrid(i, j)
+        this.drawSubGrid(i, j)
       }
     }
   }
 
-  subgrid(i, j) {
-    let subdiv = this.size / this.subcells
-    let di = subdiv * i
-    let dj = subdiv * j
-    let dx = this.x - di
-    let dy = this.y - dj
+  drawSubGrid(i, j) {
+    let dx = this.x - (this.subunit * i)
+    let dy = this.y - (this.subunit * j)
     this.drawPoints(dx, dy)
-    this.drawLines(dx, dy, subdiv)
-    this.drawCircles(dx, dy, subdiv)
-    this.drawCircles(dx, dy, subdiv, true)
+    this.drawLines(dx, dy)
+    this.drawCircles(dx, dy)
+    this.drawCircles(dx, dy, true)
   }
 
-  drawLines(dx, dy, subdiv) {
-    strokeWeight(0.3)
-    stroke(255)
-    line(dx, dy, dx + subdiv, dy)
-    line(dx, dy, dx, dy + subdiv)
+  drawLines(dx, dy) {
+    strokeWeight(1)
+    stroke(124, 124, 124)
+    line(dx, dy, dx + this.subunit, dy)
+    line(dx, dy, dx, dy + this.subunit)
   }
 
   drawPoints(dx, dy) {
     strokeWeight(5)
-    stroke(255, 0, 0)
+    stroke(248, 56, 0)
     point(dx, dy)
   }
 
-  drawCircles(dx, dy, subdiv, corner = false) {
-    strokeWeight(0.4)
+  drawCircles(dx, dy, corner = false) {
+    strokeWeight(0.7)
     noFill()
     if (corner) {
+      stroke(164, 228, 252)
       ellipseMode(CORNER)
-      stroke(0, 100, 255)
     } else {
-      stroke(0, 255, 0)
+      stroke(0, 168, 68)
       ellipseMode(CENTER)
     }
-    ellipse(dx, dy, subdiv, subdiv)
+    ellipse(dx, dy, this.subunit, this.subunit)
   }
 }
